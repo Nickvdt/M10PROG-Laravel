744d2ad4e4689d35019f0447c6cbc6632aa86afb
Nick <32934@ma-web.nl>
2023-12-07 11:01:19 +0100

    PHP migrations

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="preconnect" href="https://fonts.bunny.net%22%3E/" />
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link href="/dist/app.css" rel="stylesheet">
     <title>Contact</title>
 
     <style>
